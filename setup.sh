c41f78d17d27e8bee0ab828f3b3d6c5ea9d9894c
Erick Carvajal <neocarvajal@gmail.com>
2024-09-20 00:48:22 -0400

    add setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Agregar Rust y el CLI al PATH
-echo 'export PATH="$HOME/.local/share/sol_cli/bin:$PATH"' >> $HOME/.bashrc
+# Crear la estructura de directorios para los ejecutables del CLI
+mkdir -p "$HOME/.local/share/sol_cli/bin"
+
+# Agregar el CLI al PATH
+echo 'export PATH="$HOME/.local/share/my_sol_cli/bin:$PATH"' >> $HOME/.bashrc
 
 # Para que los cambios surtan efecto en la sesión actual
 source $HOME/.bashrc
 
-echo "Rutas agregadas al PATH. Por favor, reinicia tu terminal o ejecuta 'source ~/.bashrc' para aplicar los cambios."
+echo "Estructura de directorios creada y rutas agregadas al PATH."
+echo "Por favor, reinicia tu terminal o ejecuta 'source ~/.bashrc' para aplicar los cambios."
 
